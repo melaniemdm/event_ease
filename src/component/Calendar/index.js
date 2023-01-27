ba4d18256d0356b1b5d39d2c5bbfb94b0a035051
melaniemdm <melanieborja.pro@gmail.com>
2023-01-27 16:46:57 +0100

    feature(js & css) calendrier conception

---
+++ src/component/Calendar/index.js
@@ -0,0 +1,25 @@
+import './style.scss';
+
+export default function Calendar(){
+    return <div className="calendarStyle" >
+        <div className="calendarContainer">
+<h1>Mon calendier</h1><br></br>
+<div className="calendar">calendrier </div>
+</div>
+<br></br>
+<h1>Mes Events du jour</h1><br></br>
+<table className="event1">
+    <tr><th>Heure</th><th>Objet</th></tr>
+    <tr><td>9H00</td><td>ceci est un test</td></tr>
+</table>
+<br></br><h1>Mes Events à venir</h1><br></br>
+<table className="event1">
+    <tr><th>Date</th><th>Heure</th><th>Objet</th></tr>
+    <tr><td>28/02</td><td>9H00</td><td>ceci est un test</td></tr>
+    <tr><td>29/02</td><td>12H00</td><td>ceci est un test</td></tr>
+</table>
+    </div>
+
+
+
+}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,10 @@
+import Calendar from '../component/Calendar';
 import Header from '../component/Header';
 import Navigate from '../component/Navigate';
 export default function Profile(){
     return <div>
         <Header/>
         <Navigate/>
+     <Calendar/>   
     </div>
 }
