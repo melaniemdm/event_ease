5cfb266c86b80b5bf4cca48af27d5696d732c6ea
melaniemdm <melanieborja.pro@gmail.com>
2023-01-27 12:02:30 +0100

    refacto

--- src/component/Navigate/index.js
+++ src/component/Navigate/index.js
@@ -2,10 +2,10 @@ import './style.scss';
 
 export default function Header(){
     return <nav className="containerNavigate"><ul className="navigate">
-    <li className="navItem"><p>Home</p></li>
-    <li className="navItem"><p>Profil</p></li>
-    <li className="navItem"><p>Event</p></li>
     <li className="navItem"><p>Setting</p></li>
+    <li className="navItem"><p>Event</p></li>
+    <li className="navItem"><p>Profil</p></li>
+    <li className="navItem"><p>Home</p></li>
   </ul></nav>
 }
 
