d7253c8a832420a688f806eefdd06d765b891fc9
melaniemdm <melanieborja.pro@gmail.com>
2023-01-30 12:10:08 +0100

    refacto

--- src/component/Calendar/index.js
+++ src/component/Calendar/index.js
@@ -6,13 +6,13 @@ export default function Calendar(){
 <h1>Mon calendier</h1><br></br>
 <div className="calendar">calendrier </div>
 </div>
-<br></br>
-<h1>Mes Events du jour</h1><br></br>
+<br key={1}></br>
+<h1>Mes Events du jour</h1><br key={2}></br>
 <table className="event1">
     <tr><th>Heure</th><th>Objet</th></tr>
     <tr><td>9H00</td><td>ceci est un test</td></tr>
 </table>
-<br></br><h1>Mes Events à venir</h1><br></br>
+<br></br><h1>Mes Events à venir</h1><br key={3}></br>
 <table className="event1">
     <tr><th>Date</th><th>Heure</th><th>Objet</th></tr>
     <tr><td>28/02</td><td>9H00</td><td>ceci est un test</td></tr>
