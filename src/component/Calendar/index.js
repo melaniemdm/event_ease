a79fa41f0d92b428b7ef20c0c78cd9fe3a5bdd7a
melaniemdm <melanieborja.pro@gmail.com>
2023-01-30 16:17:46 +0100

    feature(js & css) : calendar fix(js) cardEvents

--- src/component/Calendar/index.js
+++ src/component/Calendar/index.js
@@ -1,22 +1,130 @@
+import { useEffect } from 'react';
 import './style.scss';
 
+
+let currentMonth;
+let calendarDays;
+let today = new Date();
+let date = new Date();
+
+today.setHours(0,0,0,0);
+
+
+
+function renderCalendar(){
+    const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
+    const totalMonthDay = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
+    const startWeekDay = new Date(date.getFullYear(),date.getMonth(),1).getDay();
+    calendarDays = document.querySelector(".calendar-days");
+    calendarDays.innerHTML = "";
+
+    let totalCalendarDay = 6 * 7;
+    for (let i = 0; i < totalCalendarDay; i++) {
+        let day = i-startWeekDay;
+
+        if(i <= startWeekDay){
+            // adding previous month days
+            calendarDays.innerHTML += `<div class='padding-day'>${prevLastDay-i}</div>`;
+        }else if(i <= startWeekDay+totalMonthDay){
+            // adding this month days
+            date.setDate(day);
+            date.setHours(0,0,0,0);
+            
+            let dayClass = date.getTime()===today.getTime() ? 'current-day' : 'month-day';
+            console.log(dayClass);
+            calendarDays.innerHTML += `<div class='${dayClass}'>${day}</div>`;
+        }else{
+            // adding next month days
+            calendarDays.innerHTML += `<div class='padding-day'>${day-totalMonthDay}</div>`;
+        }
+    }
+}
+
+
 export default function Calendar(){
+    useEffect(()=>{
+        renderCalendar();  
+        currentMonth = document.querySelector(".current-month");
+        if(currentMonth){
+            currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
+            }
+        
+document.querySelectorAll(".month-btn").forEach(function (element) {
+	element.addEventListener("click", function () {
+		date = new Date(currentMonth.textContent);
+        date.setMonth(date.getMonth() + (element.classList.contains("prev") ? -1 : 1));
+		currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
+		renderCalendar();
+	});
+});
+
+document.querySelectorAll(".btn").forEach(function (element) {
+	element.addEventListener("click", function () {
+        let btnClass = element.classList;
+        date = new Date(currentMonth.textContent);
+        if(btnClass.contains("today"))
+            date = new Date();
+        else if(btnClass.contains("prev-year"))
+            date = new Date(date.getFullYear()-1, 0, 1);
+        else
+            date = new Date(date.getFullYear()+1, 0, 1);
+        
+		currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
+		renderCalendar();
+	});
+});   
+     })
     return <div className="calendarStyle" >
         <div className="calendarContainer">
 <h1>Mon calendier</h1><br></br>
-<div className="calendar">calendrier </div>
+<div className="calendar">
+    
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet" />
+<div className="card">
+  <div className="calendar-toolbar">
+    <button className="prev month-btn"><i className="fas fa-chevron-left"></i></button>
+    <div className="current-month"></div>
+    <button className="next month-btn"><i className="fas fa-chevron-right"></i></button>
+  </div>
+  <div className="calendar">
+    <div className="weekdays">
+      <div className="weekday-name">Sa</div>
+      <div className="weekday-name">Su</div>
+      <div className="weekday-name">Mo</div>
+      <div className="weekday-name">Tu</div>
+      <div className="weekday-name">We</div>
+      <div className="weekday-name">Th</div>
+      <div className="weekday-name">Fr</div>
+    </div>
+    <div className="calendar-days"></div>
+  </div>
+  <div className="goto-buttons">
+    <button type="button" className="btn prev-year">Prev Year</button>
+    <button type="button" className="btn today">Today</button>
+    <button type="button" className="btn next-year">Next Year</button>
+  </div>
+</div>
+       
+    
+    
+     </div>
 </div>
 <br key={1}></br>
+
 <h1>Mes Events du jour</h1><br key={2}></br>
 <table className="event1">
+<tbody>
     <tr><th>Heure</th><th>Objet</th></tr>
     <tr><td>9H00</td><td>ceci est un test</td></tr>
+</tbody>
 </table>
 <br></br><h1>Mes Events à venir</h1><br key={3}></br>
 <table className="event1">
+<tbody>
     <tr><th>Date</th><th>Heure</th><th>Objet</th></tr>
     <tr><td>28/02</td><td>9H00</td><td>ceci est un test</td></tr>
     <tr><td>29/02</td><td>12H00</td><td>ceci est un test</td></tr>
+    </tbody>
 </table>
     </div>
 
--- src/component/CardEvents/index.js
+++ src/component/CardEvents/index.js
@@ -4,46 +4,46 @@ export default function CardEvents(){
     return <div className="cardEvents">
         <h1 className="titleEvent">Mes events</h1>
 
-  <div class="ticketContainer">
-  <div class="ticket">
-    <div class="ticketTitle">Benevola ASMAA</div>
+  <div className="ticketContainer">
+  <div className="ticket">
+    <div className="ticketTitle">Benevola ASMAA</div>
    
-    <div class="ticketDetail">
+    <div className="ticketDetail">
       <div>Objet : &ensp; tototototo</div>
       <div>Nbre de personne:&nbsp; 5</div>
       <div>Time:&emsp; 19:20</div>
     </div>
-    <div class="ticketRip">
-      <div class="circleLeft"></div>
-      <div class="ripLine"></div>
-      <div class="circleRight"></div>
+    <div className="ticketRip">
+      <div className="circleLeft"></div>
+      <div className="ripLine"></div>
+      <div className="circleRight"></div>
     </div>
-    <div class="ticketSubDetail">
-      <div class="code">LO-2314XXX</div>
-      <div class="date"> Jan 14<sup>th</sup> 2023</div>
+    <div className="ticketSubDetail">
+      <div className="code">LO-2314XXX</div>
+      <div className="date"> Jan 14<sup>th</sup> 2023</div>
     </div>
   </div>
 
 </div>
   
 <h1 className="titleEvent">Mes events en attente d'approbation</h1>
-<div class="ticketContainer">
-  <div class="ticket">
-    <div class="ticketTitle">Benevola ASMAA</div>
+<div className="ticketContainer">
+  <div className="ticket">
+    <div className="ticketTitle">Benevola ASMAA</div>
    
-    <div class="ticketDetail">
+    <div className="ticketDetail">
       <div>Objet : &ensp; tototototo</div>
       <div>Nbre de personne:&nbsp; 5</div>
       <div>Time:&emsp; 19:20</div>
     </div>
-    <div class="ticketRip">
-      <div class="circleLeft"></div>
-      <div class="ripLine"></div>
-      <div class="circleRight"></div>
+    <div className="ticketRip">
+      <div className="circleLeft"></div>
+      <div className="ripLine"></div>
+      <div className="circleRight"></div>
     </div>
-    <div class="ticketSubDetail">
-      <div class="code">LO-2314XXX</div>
-      <div class="date"> Jan 14<sup>th</sup> 2023</div>
+    <div className="ticketSubDetail">
+      <div className="code">LO-2314XXX</div>
+      <div className="date"> Jan 14<sup>th</sup> 2023</div>
     </div>
   </div>
 
